a9be5c8723551b5c079fad61f02dc15052a829e9
amaraoussama94 <wassa36@yahoo.fr>
2022-11-04 16:11:59 +0100

    fixing  speed of  ball

--- Ball.hpp
+++ Ball.hpp
@@ -8,7 +8,7 @@ class Ball
     private:
         Vector2f m_Position;
         RectangleShape m_Shape;
-        float m_Speed = 300.0f;
+        float m_Speed = 1000.0f;
         float m_DirectionX = .2f;
         float m_DirectionY = .2f;
 
